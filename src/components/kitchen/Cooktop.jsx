32048b0f99ae70d77d94f6e187759672f27181b6
ArneSamson <arnesamson007@gmail.com>
2024-02-20 12:02:15 +0100

    feat: add position props to Cooktop

--- src/components/kitchen/Cooktop.jsx
+++ src/components/kitchen/Cooktop.jsx
@@ -5,9 +5,11 @@ import { WoodMaterial } from '../Materials.jsx'
 
 
 
-export default function Cooktop(){
+export default function Cooktop({position}){
     return <>
-        <mesh>
+        <mesh
+            position={position}
+        >
             <cylinderGeometry args={[1, 1, 2, 32]}/>
             <WoodMaterial/>
         </mesh>
