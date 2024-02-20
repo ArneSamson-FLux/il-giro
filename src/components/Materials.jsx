7fdab4008576c3addb7c0a2bbbe4d5a6a7c65b12
ArneSamson <arnesamson007@gmail.com>
2024-02-20 12:01:31 +0100

    feat: fetch texture from url that is stored in data.json

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -1,27 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { TextureLoader } from 'three';
+import React, { useEffect, useState, useRef } from 'react';
+import { SRGBColorSpace, TextureLoader } from 'three';
 import { useLoader } from '@react-three/fiber';
+import { useTexture } from '@react-three/drei';
 
 export function WoodMaterial() {
 
-    const [textureUrl, setTexture] = useState();
+    const material = useRef();
 
     useEffect(() => {
         fetch('/data.json')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                setTexture(data.woodMaterial.textureUrl);
+
+                const textureloader = new TextureLoader();
+                textureloader.load(data.woodMaterial.textureUrl, (t) => {
+                    t.colorSpace = SRGBColorSpace;
+                    t.needsUpdate = true;
+                    material.current.map = t;
+                    material.current.needsUpdate = true;
+                });
+
             })  
             .catch(error => console.error('Error fetching texture:', error));
     }, []);
 
-    const texture = textureUrl ? useLoader(TextureLoader, textureUrl) : null;
 
     return <>
-        <meshStandardMaterial
-            map={texture}
-            // color={0x8B4513}
+        <meshBasicMaterial
+            ref={material}   
+            roughness={0.5}
+            metalness={0.5}
+            wrapS="ClampToEdgeWrapping"
+            wrapT="ClampToEdgeWrapping"
+            magFilter="LinearFilter"
         />
     </>
 }
