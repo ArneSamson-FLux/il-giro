92231829ed0211513b04944e1fcc419e8e04195d
ArneSamson <arnesamson007@gmail.com>
2024-02-21 10:34:16 +0100

    style: remove unnecessary comments

--- src/store/useConfig.jsx
+++ src/store/useConfig.jsx
@@ -7,40 +7,18 @@ export default create((set) => {
     fetch('/src/data/data.json')
         .then(response => response.json())
         .then(data => {
-            // materialData = data.materials;
-            // set({
-            //     woodMaterialUrl: materialData.woodMaterial.url,
-            //     metalMaterialUrl: materialData.metalMaterial.url,
-            //     leatherMaterialUrl: materialData.leatherMaterial.url
-            // });
-
-            materialData = data.materials;
-
             materialData = data.materials;
             const materialUrls = Object.values(materialData).map(material => material.url);
-            console.log(materialUrls);
             set({ materialUrls });
-
-            // // Loop over each material in the JSON and create a store item for it
-            // Object.entries(materialData).forEach(([materialKey, materialValue]) => {
-            //     set((state) => ({
-            //         [`${materialKey}Url`]: materialValue.url,
-            //         ...state // Ensure to merge with existing state
-            //     }));
-            // });
-
         })
         .catch(error => console.error('Error fetching texture:', error));
 
     return{
         //materials
-        // woodMaterialUrl: null,
-        // metalMaterialUrl: null,
-        // leatherMaterialUrl: null,
         materialUrls: [],
 
         //config settings
-        sinkAmount: 1,
+        sinkAmount: 2,
         cooktopAmount: 1,
         towerAmount: 1
 
