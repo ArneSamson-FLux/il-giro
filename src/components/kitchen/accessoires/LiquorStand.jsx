f5921045f91efe860782b81389ff6b7219b798a4
ArneSamson <arnesamson007@gmail.com>
2024-02-26 16:46:01 +0100

    feat: give material to liquor rack

--- src/components/kitchen/accessoires/LiquorStand.jsx
+++ src/components/kitchen/accessoires/LiquorStand.jsx
@@ -2,7 +2,25 @@ import React, { useRef } from 'react';
 import * as THREE from 'three'
 import { useTexture, useGLTF } from '@react-three/drei'
 
-export default function LiquorStand({props}){
+export default function LiquorStand({props, materialUrl}){
+
+    const albedoTexture = useTexture(materialUrl+"albedo.jpg");
+    const normalTexture = useTexture(materialUrl+"normal.jpg");
+    const roughnessTexture = useTexture(materialUrl+"roughness.jpg");
+    const metallnesTexture = useTexture(materialUrl+"metallic.jpg");
+
+    albedoTexture.colorSpace = THREE.SRGBColorSpace;
+
+    const material = new THREE.MeshStandardMaterial({
+        map: albedoTexture,
+        normalMap: normalTexture,
+        roughnessMap: roughnessTexture,
+        metalnessMap: metallnesTexture,
+        metalness: 1,
+        roughness: 0
+    });
+
+
     const { nodes, materials } = useGLTF("./models/liquorStand.glb");
     return (
         <group {...props} dispose={null}>
@@ -10,7 +28,7 @@ export default function LiquorStand({props}){
                 castShadow
                 receiveShadow
                 geometry={nodes.liquorStand.geometry}
-                material={nodes.liquorStand.material}
+                material={material}
                 position={[0, 1.43, 0]}
             />
         </group>
