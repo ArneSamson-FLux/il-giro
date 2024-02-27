9ca4ec93780607407ec874a4b8f826096630494a
ArneSamson <arnesamson007@gmail.com>
2024-02-27 08:56:33 +0100

    feat: add all variables from debug UI to store and setfunctions

--- src/store/useConfig.jsx
+++ src/store/useConfig.jsx
@@ -17,10 +17,60 @@ export default create((set) => {
         //materials
         materialUrls: [],
 
-        //config settings
+        //config settings_______________________________________________________________________________
         sinkAmount: 1,
         cooktopAmount: 1,
-        towerAmount: 1
+        towerAmount: 1,
+
+        sinkMaterial: null,
+        cookTopMaterial: null,
+        towerMaterial: null,
+
+        sinkBevelled: false,
+        cooktopBevelled: false,
+        towerBevelled: true,
+
+        tapMaterial: null,
+        tapType: 'tap1',
+
+        sinkBowlMaterial: null,
+
+        stoveType: 'gas',
+
+        applianceType: 'oven',
+
+        towerAccessoryMaterial: null,
+
+        //Actions_______________________________________________________________________________________
+        setSinkAmount: (amount) => set({sinkAmount: amount}),
+        setCooktopAmount: (amount) => set({cooktopAmount: amount}),
+        setTowerAmount: (amount) => set({towerAmount: amount}),
+
+        //material
+        setSinkMaterial: (material) => set({sinkMaterial: material}),
+        setCooktopMaterial: (material) => set({cooktopMaterial: material}),
+        setTowerMaterial: (material) => set({towerMaterial: material}),
+
+        //bevel
+        setSinkBevelled: (bevelled) => set({sinkBevelled: bevelled}),
+        setCooktopBevelled: (bevelled) => set({cooktopBevelled: bevelled}),
+        setTowerBevelled: (bevelled) => set({towerBevelled: bevelled}),
+
+        //tap
+        setTapMaterial: (material) => set({tapMaterial: material}),
+        setTapType: (type) => set({tapType: type}),
+
+        //sink
+        setSinkBowlMaterial: (material) => set({sinkBowlMaterial: material}),
+
+        //stove
+        setStoveType: (type) => set({stoveType: type}),
+
+        //appliance
+        setApplianceType: (type) => set({applianceType: type}),
+
+        //tower accessory
+        setTowerAccessoryMaterial: (material) => set({towerAccessoryMaterial: material})
 
     }
 })
