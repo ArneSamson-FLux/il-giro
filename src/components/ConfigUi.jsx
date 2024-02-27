588435bcbf7058355b6ca3b69abc4725131db228
ArneSamson <arnesamson007@gmail.com>
2024-02-27 09:06:53 +0100

    feat: add all changing variables to UI and make them editable

--- src/components/ConfigUi.jsx
+++ src/components/ConfigUi.jsx
@@ -32,7 +32,82 @@ export default function ConfigUi() {
                     {materialUrls.map((url, index) => <option key={index} value={url}>{url}</option>)}
                 </select>
             </div>
+
+            <div>
+                <p>Cooktop Material: {cooktopMaterial}</p>
+                <select onChange={(e) => setCooktopMaterial(e.target.value)}>
+                    {materialUrls.map((url, index) => <option key={index} value={url}>{url}</option>)}
+                </select>
+            </div>
+
+            <div>
+                <p>Tower Material: {towerMaterial}</p>
+                <select onChange={(e) => setTowerMaterial(e.target.value)}>
+                    {materialUrls.map((url, index) => <option key={index} value={url}>{url}</option>)}
+                </select>
+            </div>
+
+            <div>
+                <p>Sink Bevelled: {sinkBevelled}</p>
+                <input type="checkbox" onChange={(e) => setSinkBevelled(e.target.checked)} />
+            </div>
+
+            <div>
+                <p>Cooktop Bevelled: {cooktopBevelled}</p>
+                <input type="checkbox" onChange={(e) => setCooktopBevelled(e.target.checked)} />
+            </div>
+
+            <div>
+                <p>Tower Bevelled: {towerBevelled}</p>
+                <input type="checkbox" onChange={(e) => setTowerBevelled(e.target.checked)} />
+            </div>
             
+            <div>
+                <p>Tap Material: {tapMaterial}</p>
+                <select onChange={(e) => setTapMaterial(e.target.value)}>
+                    {materialUrls.map((url, index) => <option key={index} value={url}>{url}</option>)}
+                </select>
+            </div>
+
+            <div>
+                <p>Tap Type: {tapType}</p>
+                <select onChange={(e) => setTapType(e.target.value)}>
+                    <option value="tap1">Tap 1</option>
+                    <option value="tap2">Tap 2</option>
+                </select>
+            </div>
+
+            <div>
+                <p>Sink Bowl Material: {sinkBowlMaterial}</p>
+                <select onChange={(e) => setSinkBowlMaterial(e.target.value)}>
+                    {materialUrls.map((url, index) => <option key={index} value={url}>{url}</option>)}
+                </select>
+            </div>
+
+            <div>
+                <p>Stove Type: {stoveType}</p>
+                <select onChange={(e) => setStoveType(e.target.value)}>
+                    <option value="gas">Gas</option>
+                    <option value="electric">Electric</option>
+                </select>
+            </div>
+
+            <div>
+                <p>Appliance Type: {applianceType}</p>
+                <select onChange={(e) => setApplianceType(e.target.value)}>
+                    <option value="oven">Oven</option>
+                    <option value="microwave">Microwave</option>
+                </select>
+            </div>
+
+            <div>
+                <p>Tower Accessory Material: {towerAccessoryMaterial}</p>
+                <select onChange={(e) => setTowerAccessoryMaterial(e.target.value)}>
+                    {materialUrls.map((url, index) => <option key={index} value={url}>{url}</option>)}
+                </select>
+            </div>
+
+
         </Html>
     );
 }
