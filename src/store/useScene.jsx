2288292fc803bd436f0d31c9968cc5fa4f3ce7a5
ArneSamson <arnesamson007@gmail.com>
2024-02-28 09:55:29 +0100

    test: try hovering state on objects in scene

--- src/store/useScene.jsx
+++ src/store/useScene.jsx
@@ -5,9 +5,12 @@ export default create((set) => {
     return {
         cameraCoords: [0, 2, 4],
 
-        cameraFocus: [0, 0, 0],
+        cameraFocus: [0, 1, 0],
+
+        isHovering: false,
 
         setCameraCoords: (coords) => set({ cameraCoords: coords }),
         setCameraFocus: (coords) => set({ cameraFocus: coords }),
+        setIsHovering: (bool) => set({ isHovering: bool }),
     }
 });
