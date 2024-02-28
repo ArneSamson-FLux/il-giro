4d69f513f1ea007c73fcd134a8d4948dc315a1d1
ArneSamson <arnesamson007@gmail.com>
2024-02-28 09:21:57 +0100

    feat: add cameraFocus point

--- src/store/useScene.jsx
+++ src/store/useScene.jsx
@@ -5,6 +5,9 @@ export default create((set) => {
     return {
         cameraCoords: [0, 2, 4],
 
+        cameraFocus: [0, 0, 0],
+
         setCameraCoords: (coords) => set({ cameraCoords: coords }),
+        setCameraFocus: (coords) => set({ cameraFocus: coords }),
     }
 });
