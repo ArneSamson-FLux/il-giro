c96d0dc574ffedb0140cb306e08599f5eeeb8f12
ArneSamson <arnesamson007@gmail.com>
2024-03-04 08:54:59 +0100

    chore: make movement of shadow less harsh

--- src/components/lighting&shadows/ShadowPlanes.jsx
+++ src/components/lighting&shadows/ShadowPlanes.jsx
@@ -15,11 +15,11 @@ export function BakePlane({props}){
 
     useFrame(() => {
         if(bigPlaneRef.current){
-            bigPlaneRef.current.position.y =  - bigPlaneRef.current.parent.position.y
-            bigPlaneRef.current.position.z =  - bigPlaneRef.current.parent.position.y
+            bigPlaneRef.current.position.y =  - bigPlaneRef.current.parent.position.y;
+            bigPlaneRef.current.position.z =  - bigPlaneRef.current.parent.position.y / 3;
             if(bigPlaneRef.current.children[0].material){
                 bigPlaneRef.current.children[0].material.opacity = (1 - (bigPlaneRef.current.parent.position.y * 1.5)) * 1;
-            }
+            };
         }
     });
     
@@ -27,8 +27,8 @@ export function BakePlane({props}){
         <group 
             ref={bigPlaneRef}
             {...props} 
-            dispose={null
-        }>
+            dispose={null}
+        >
             <mesh
                 geometry={nodes.Plane002.geometry}
                 renderOrder={1}
@@ -58,11 +58,11 @@ export function BakePlaneSmall({props}){
 
     useFrame(() => {
         if(smallPlaneRef.current){
-            smallPlaneRef.current.position.y =  - smallPlaneRef.current.parent.position.y
-            smallPlaneRef.current.position.z =  - smallPlaneRef.current.parent.position.y
+            smallPlaneRef.current.position.y =  - smallPlaneRef.current.parent.position.y;
+            smallPlaneRef.current.position.z =  - smallPlaneRef.current.parent.position.y / 3;
             if(smallPlaneRef.current.children[0].material){
                 smallPlaneRef.current.children[0].material.opacity = (1 - (smallPlaneRef.current.parent.position.y * 1.5)) * 1;
-            }
+            };
         }
     }
     );
