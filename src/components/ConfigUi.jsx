1375ec3ade2bbb10b4dc49ed0cb2d510e0e77963
ArneSamson <arnesamson007@gmail.com>
2024-03-05 09:52:14 +0100

    feat: add styling to UI buttons

--- src/components/ConfigUi.jsx
+++ src/components/ConfigUi.jsx
@@ -155,7 +155,7 @@ export default function ConfigUi() {
                     <button
                         onClick={handleZoom}
                     >
-                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" className='zoom-out__image'>
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" className='zoom-out__image '>
                             <path d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             <path d="M19 19L14.65 14.65" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             <path d="M6 9H12" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -164,12 +164,13 @@ export default function ConfigUi() {
                 </div>
 
                 <div
-                    className='config-ui__move'
+                    // className='config-ui__move'
+                    className={dragMode ? 'config-ui__move--active' : 'config-ui__move'}
                 >
                     <button
                         onClick={handleDragMode}
                     >
-                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" className='move__image'>
+                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" className={dragMode ? 'move__image--active' : 'move__image'}>
                             <path d="M4 8L1 11L4 14" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             <path d="M8 4L11 1L14 4" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             <path d="M14 18L11 21L8 18" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -194,6 +195,8 @@ export default function ConfigUi() {
                     >
 
                         <button
+                            style={currentPage === 0 ? {opacity: 0.4} : {opacity: 1}}
+                            className={currentPage === 0 ? 'config-ui__nav__button--disabled' : ''}
                             onClick={handleBack}
                         >
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -203,6 +206,8 @@ export default function ConfigUi() {
                         </button>
   
                         <button
+                            style={currentPage === 3 ? {opacity: 0.4} : {opacity: 1}}
+                            className={currentPage === 3 ? 'config-ui__nav__button--disabled' : ''}
                             onClick={handleNext}
                         >
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
