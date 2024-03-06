02cb43a604248316b74b64d7f3d305b715425f16
ArneSamson <arnesamson007@gmail.com>
2024-03-06 14:35:58 +0100

    test: tweak parameters on reflective plane

--- src/components/lighting&shadows/ReflectivePlane.jsx
+++ src/components/lighting&shadows/ReflectivePlane.jsx
@@ -5,10 +5,26 @@ export default function ReflectivePlane({props}){
 
     return <>
         <mesh
+            name='reflectivePlane'
             {...props}
         >
             <planeGeometry/>
             <MeshReflectorMaterial
+                // blur={[400, 100]}
+                // color={0xFAF9F6}
+                // // mixStrength={1}
+                // // mirror={0.3}
+                // // resolution={1024}
+                // // depthScale={0}
+                // // reflectorOffset={0}
+                // resolution={1024}
+                // mixBlur={1}
+                // mixStrength={7}
+                // roughness={0}
+                // mirror={0.5}
+                // depthScale={1}
+                // minDepthThreshold={0.5}
+
                 color={0xFAF9F6}
                 mixStrength={1}
                 mirror={0.3}
