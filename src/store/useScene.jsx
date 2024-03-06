2f329221bb0eb5665ad8c30eefc01ae977c85b0f
ArneSamson <arnesamson007@gmail.com>
2024-03-06 10:38:36 +0100

    feat: add bool to check if focussed on an island

--- src/store/useScene.jsx
+++ src/store/useScene.jsx
@@ -3,14 +3,18 @@ import { create } from 'zustand';
 export default create((set) => {
 
     return {
-        cameraCoords: [0, 2, 4],
+        cameraCoords: [0, 2, 0],
 
         cameraFocus: [0, 1, 0],
 
         isHovering: false,
 
+        isFocussedOnIsland: false,
+
         setCameraCoords: (coords) => set({ cameraCoords: coords }),
         setCameraFocus: (coords) => set({ cameraFocus: coords }),
         setIsHovering: (bool) => set({ isHovering: bool }),
+        setIsFocussedOnIsland: (bool) => set({ isFocussedOnIsland: bool }),
+
     }
 });
