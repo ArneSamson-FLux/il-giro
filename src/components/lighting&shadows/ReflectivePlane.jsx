7eae7e5cf983716d58d8a6ac7f9d34277218edca
ArneSamson <arnesamson007@gmail.com>
2024-03-15 13:20:01 +0100

    chore: tweak reflectiveplane

--- src/components/lighting&shadows/ReflectivePlane.jsx
+++ src/components/lighting&shadows/ReflectivePlane.jsx
@@ -14,33 +14,16 @@ export default function ReflectivePlane({props}){
         >
             <circleGeometry/>
             <MeshReflectorMaterial
-                // blur={[400, 100]}
-                // color={0xFAF9F6}
-                // // mixStrength={1}
-                // // mirror={0.3}
-                // // resolution={1024}
-                // // depthScale={0}
-                // // reflectorOffset={0}
-                // resolution={1024}
-                // mixBlur={1}
-                // mixStrength={7}
-                // roughness={0}
-                // mirror={0.5}
-                // depthScale={1}
-                // minDepthThreshold={0.5}
-
-                // color={0xF9f9f9}
                 color={0xffffff}
-                // color={0xd3d3d3}
-                mixStrength={1}
-                mirror={0.3}
+                blur={[300, 100]}
+                mixBlur={1}
+                mixStrength={40}
                 resolution={1024}
-                depthScale={0}
-                reflectorOffset={0}
-
-                // alphaMap={alphaMap}
-                // transparent={true}
-                // depthWrite={false}
+                roughness={0.2}
+                metalness={0}
+                depthScale={0.8}
+                minDepthThreshold={0.2}
+                maxDepthThreshold={1.4}
             />
         </mesh>
     </>
