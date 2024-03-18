7782c14f59044d9cfe45d1dfb808389a72d72886
ArneSamson <arnesamson007@gmail.com>
2024-03-18 12:39:20 +0100

    chore: updated tap 2 to deliverd model

--- src/components/kitchen/accessoires/Tap2.jsx
+++ src/components/kitchen/accessoires/Tap2.jsx
@@ -5,16 +5,13 @@ import { useTexture, useGLTF } from '@react-three/drei'
 export default function Tap1({materialUrl, bevelled, props}){
 
     const albedoTexture = useTexture(materialUrl+"albedo.jpg");
-    const normalTexture = useTexture(materialUrl+"normal.jpg");
-    const roughnessTexture = useTexture(materialUrl+"roughness.jpg");
     const metallnesTexture = useTexture(materialUrl+"metallic.jpg");
 
     albedoTexture.colorSpace = THREE.SRGBColorSpace;
 
     const material = new THREE.MeshStandardMaterial({
         map: albedoTexture,
-        // normalMap: normalTexture,
-        // roughnessMap: roughnessTexture,
+
         metalnessMap: metallnesTexture,
         metalness: 1,
         roughness: 0
@@ -29,46 +26,12 @@ export default function Tap1({materialUrl, bevelled, props}){
             {...props} 
             dispose={null}
         >
-            <group 
-                name="tap2-group"
-                position={[0, 0.973, 0]}
-            >
-                <mesh
-                    name='tap2-mesh-1'
-                    castShadow
-                    receiveShadow
-                    geometry={nodes["tap2-Quooker_1"].geometry}
-                    material={material}
-                />
-                <mesh
-                    name='tap2-mesh-2'
-                    castShadow
-                    receiveShadow
-                    geometry={nodes["tap2-Quooker_2"].geometry}
-                    material={material}
-                />
-                <mesh
-                    name='tap2-mesh-3'
-                    castShadow
-                    receiveShadow
-                    geometry={nodes["tap2-Quooker_3"].geometry}
-                    material={materials["iMeshh Red Plastic"]}
-                />
-                <mesh
-                    name='tap2-mesh-4'
-                    castShadow
-                    receiveShadow
-                    geometry={nodes["tap2-Quooker_4"].geometry}
-                    material={materials["iMeshh Plastic"]}
-                />
-                <mesh
-                    name='tap2-mesh-5'
-                    castShadow
-                    receiveShadow
-                    geometry={nodes["tap2-Quooker_5"].geometry}
-                    material={material}
-                />
-            </group>
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Bridge.geometry}
+                material={material}
+            />
         </group>
     );
 }
