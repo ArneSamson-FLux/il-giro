c6797fc41e948a39b4fb77861e02612d98f25e36
ArneSamson <arnesamson007@gmail.com>
2024-03-18 12:35:50 +0100

    chore: update tap to delivered tap

--- src/components/kitchen/accessoires/Tap1.jsx
+++ src/components/kitchen/accessoires/Tap1.jsx
@@ -2,25 +2,21 @@ import React, { useRef } from 'react';
 import * as THREE from 'three'
 import { useTexture, useGLTF } from '@react-three/drei'
 
-export default function Tap1({materialUrl, bevelled, props}){
+export default function Tap1({materialUrl, props}){
 
     const albedoTexture = useTexture(materialUrl+"albedo.jpg");
-    const normalTexture = useTexture(materialUrl+"normal.jpg");
-    const roughnessTexture = useTexture(materialUrl+"roughness.jpg");
     const metallnesTexture = useTexture(materialUrl+"metallic.jpg");
 
     albedoTexture.colorSpace = THREE.SRGBColorSpace;
 
     const material = new THREE.MeshStandardMaterial({
         map: albedoTexture,
-        // normalMap: normalTexture,
-        // roughnessMap: roughnessTexture,
         metalnessMap: metallnesTexture,
         metalness: 1,
         roughness: 0
     });
     
-    const { nodes, materials } = useGLTF("/models//tap1.glb");
+    const { nodes, materials } = useGLTF("/models/tap1.glb");
     
     return (
         <group 
@@ -29,12 +25,20 @@ export default function Tap1({materialUrl, bevelled, props}){
             dispose={null}
         >
             <mesh
-                name="tap1-mesh"
                 castShadow
                 receiveShadow
-                geometry={nodes.Tap1.geometry}
+                geometry={nodes['3DGeom~3771_(C-3DGeom~3771_Defintion#3)'].geometry}
                 material={material}
-                position={[0, 0.973, 0]}
+                position={[-0.029, 0.874, -0.026]}
+                scale={1}
+            />
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes['3DGeom~5954_(C-3DGeom~5954_Defintion#4)'].geometry}
+                material={material}
+                position={[-0.029, 0.874, -0.026]}
+                scale={1}
             />
         </group>
     );
