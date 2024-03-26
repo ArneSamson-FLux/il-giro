2682082169f65ad70135e2a31eaa32d9bad28488
ArneSamson <arnesamson007@gmail.com>
2024-03-26 15:11:20 +0100

    fix: remove bias to remove artefacts

--- src/components/lighting&shadows/Lights.jsx
+++ src/components/lighting&shadows/Lights.jsx
@@ -2,8 +2,7 @@ import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import useShadowHelper from '../../helper/useShadowHelper.jsx'
 
-export default function Lights()
-{
+export default function Lights() {
     const lightRef = useRef()
 
     // const helper = useShadowHelper(lightRef)
@@ -12,17 +11,17 @@ export default function Lights()
         <directionalLight
             ref={lightRef}
             castShadow
-            position={ [ 0, 4, 5 ] }
-            intensity={ 4.5 }
-            shadow-mapSize={ [ 1024, 1024 ] }
-            shadow-camera-near={ 0.1 }
-            shadow-camera-far={ 10 }
-            shadow-camera-top={ 5 }
-            shadow-camera-right={ 5 }
-            shadow-camera-bottom={ - 4 }
-            shadow-camera-left={ - 5 }
-            shadow-bias={ 0.006}
-            shadow-normalBias={ 0.03 }
+            position={[0, 4, 5]}
+            intensity={4.5}
+            shadow-mapSize={[1024, 1024]}
+            shadow-camera-near={0.1}
+            shadow-camera-far={10}
+            shadow-camera-top={5}
+            shadow-camera-right={5}
+            shadow-camera-bottom={- 4}
+            shadow-camera-left={- 5}
+            shadow-bias={0.00}
+            shadow-normalBias={0}
         />
         {/* <ambientLight intensity={ 1.4 } /> */}
     </>
