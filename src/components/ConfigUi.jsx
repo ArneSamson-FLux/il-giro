757edf67a33bd890481ec887fcf99f30677361f2
ArneSamson <arnesamson007@gmail.com>
2024-03-26 14:14:38 +0100

    chore: remove commented code

--- src/components/ConfigUi.jsx
+++ src/components/ConfigUi.jsx
@@ -107,23 +107,7 @@ export default function ConfigUi() {
 
                     {currentPage === 5 && <>
 
-                        {/* <div
-                            className='config-ui__slider'
-                        >
-                            <h5>Open doors and shelves:</h5>
 
-                            <label className="config-ui__toggle">
-                                <input
-                                    type="checkbox"
-                                    checked={doorOpeningRotation === 1.5}
-                                    onChange={(e) => {
-                                        setDoorOpeningRotation(e.target.checked ? 1.5 : 0);
-                                    }}
-
-                                />
-                                <span className="config-ui__toggle-slider"></span>
-                            </label>
-                        </div> */}
                     </>}
 
                 </div>
